121b6ffaa27d68a39e532f3a153e693450ea8ae1
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-22 13:12:35 +0600

    final design done

---
+++ index.js
@@ -0,0 +1,9 @@
+let total = document.getElementById('total');
+let interview = document.getElementById('interview');
+let rejected = document.getElementById('rejected');
+
+const JobDetails = document.getElementById('job-details');
+const totalJobs = JobDetails.children.length;
+
+let interviewArray = [];
+let rejectedArray = [];
