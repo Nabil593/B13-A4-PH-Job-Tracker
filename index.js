ae57e4d7155777043f3bee1cfdea9489f3514d0a
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-23 13:05:30 +0600

    Interview  toggle almost done

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ let rejectedData = [];
 let total = document.getElementById('total');
 let interview = document.getElementById('interview');
 let rejected = document.getElementById('rejected');
-
 const allBtn = document.getElementById('all-btn');
 const interviewBtn = document.getElementById('interview-btn');
 const rejectedBtn = document.getElementById('rejected-btn');
-
-
 const mainContainer = document.querySelector('main');
-
+const filterSection = document.getElementById('filtered-section');
 const JobDetails = document.getElementById('job-details');
 const totalJobs = JobDetails.children.length;
 
@@ -22,7 +19,7 @@ function switchButtons() {
     interview.innerText = interviewData.length;
     rejected.innerText = rejectedData.length;
 }
-switchButtons();
+// switchButtons();
 
 
 // Switcg Toggle function:
@@ -39,27 +36,77 @@ function toggleStyle(id) {
 
     selected.classList.remove('bg-white', 'text-black');
     selected.classList.add('bg-[#3B82F6]', 'text-white');
+
+    if (id === 'interview-btn'){
+        JobDetails.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+    } else if (id === 'all-btn') {
+        JobDetails.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    }
 }
 
 //Main container Click function
 mainContainer.addEventListener('click', function(e) {
+
+    if(e.target.classList.contains('interv-btn')) {
+
     const mainParentNode = e.target.parentNode;
     const jobName = mainParentNode.querySelector('.job-name').innerText;
     const jobTitle = mainParentNode.querySelector('.job-title').innerText;
     const salary = mainParentNode.querySelector('.salary').innerText;
-    const jobStatus = mainParentNode.querySelector('.status').innerText;
     const jobDiscription = mainParentNode.querySelector('.discription').innerText;
     
+    let jobStatus = mainParentNode.querySelector('.status');
+    jobStatus.innerText = 'INTERVIEW';
+    jobStatus.className = 'w-30 text-center py-1.5 px-3 rounded-md bg-green-200 text-[14px] font-medium border-1 border-[#10B981] text-[#027a24]';
+
     const jobInfo = {
         jobName,
         jobTitle,
         salary,
-        jobStatus,
+        jobStatus : 'INTERVIEW',
         jobDiscription
     };
+
     
     const jobExist = interviewData.find(item => item.jobName == jobInfo.jobName);
     if (!jobExist) {
         interviewData.push(jobInfo);
+        renderInterview();
+        switchButtons();
     }
+}
 })
+
+
+
+// Render Interview Data
+function renderInterview() {
+    filterSection.innerHTML = '';
+
+    for(let inter of interviewData) {
+        console.log(inter);
+        let div = document.createElement('div');
+        div.className = 'bg-white p-6 rounded-lg flex justify-between';
+        div.innerHTML = `
+        <div>
+                <h3 class="job-name text-[#002C5C] text-[20px] font-semibold">${inter.jobName}</h3>
+                <p class="job-title text-[#64748B] text-[16px]">React Native Developer</p>
+                <p class="salary text-[#64748B] text-[14px] my-[20px]">Remote • Full-time • $130,000 - $175,000</p>
+                <p class="w-30 text-center py-1.5 px-3 rounded-md bg-green-200 text-[14px] font-medium border-1 border-[#10B981] text-[#027a24]">${inter.jobStatus}</p>
+                <p class="discription text-[#323B49] text-[14px] mt-2 mb-5">Build cross-platform mobile applications using React
+                    Native. Work on products used by millions of users worldwide.</p>
+                <button
+                    class="py-1.5 px-3 border-1 rounded-md border-[#10B981] text-[#10B981] text-[14px] font-semibold cursor-pointer">INTERVIEW</button>
+                <button
+                    class="py-1.5 px-3 border-1 rounded-md border-[#EF4444] text-[#EF4444] text-[14px] font-semibold cursor-pointer">REJECTED</button>
+            </div>
+            <div
+                class="w-9 h-9 border-1 p-2 border-[#c7c7c7] flex items-center justify-center rounded-full text-gray-500 cursor-pointer">
+                <i class="fa-regular fa-trash-can"></i>
+            </div>
+        `
+        filterSection.appendChild(div);
+    }
+}
