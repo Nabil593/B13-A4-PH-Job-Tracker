02dd6b28f83cd01579752d54d7152aa0883458fe
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-23 16:08:53 +0600

    almost toggles done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let interviewData = [];
 let rejectedData = [];
+let currentStatus = 'all';
 
 // All switch toggle buttons
 let total = document.getElementById('total');
@@ -8,6 +9,7 @@ let rejected = document.getElementById('rejected');
 const allBtn = document.getElementById('all-btn');
 const interviewBtn = document.getElementById('interview-btn');
 const rejectedBtn = document.getElementById('rejected-btn');
+// const noJobsSection = document.getElementById('empty-section');
 const mainContainer = document.querySelector('main');
 const filterSection = document.getElementById('filtered-section');
 const JobDetails = document.getElementById('job-details');
@@ -19,11 +21,12 @@ function switchButtons() {
     interview.innerText = interviewData.length;
     rejected.innerText = rejectedData.length;
 }
-// switchButtons();
+switchButtons();
 
 
 // Switcg Toggle function:
 function toggleStyle(id) {
+
     allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
     interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white');
     rejectedBtn.classList.remove('bg-[#3B82F6]', 'text-white');
@@ -33,6 +36,7 @@ function toggleStyle(id) {
     rejectedBtn.classList.add('bg-white', 'text-black');
 
     const selected = document.getElementById(id);
+    currentStatus = id;
 
     selected.classList.remove('bg-white', 'text-black');
     selected.classList.add('bg-[#3B82F6]', 'text-white');
@@ -40,9 +44,14 @@ function toggleStyle(id) {
     if (id === 'interview-btn'){
         JobDetails.classList.add('hidden');
         filterSection.classList.remove('hidden');
+        renderInterview();
     } else if (id === 'all-btn') {
         JobDetails.classList.remove('hidden');
         filterSection.classList.add('hidden');
+    } else if (id === 'rejected-btn') {
+        JobDetails.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderRejected();
     }
 }
 
@@ -74,10 +83,51 @@ mainContainer.addEventListener('click', function(e) {
     if (!jobExist) {
         interviewData.push(jobInfo);
         renderInterview();
-        switchButtons();
+    }
+    
+    rejectedData = rejectedData.filter(item => item.jobName !== jobInfo.jobName);
+    switchButtons();
+    
+    if (currentStatus == 'rejected-btn') {
+        renderRejected();
+    }
+
+
+} else if(e.target.classList.contains('reject-btn')) {
+
+    const mainParentNode = e.target.parentNode;
+    const jobName = mainParentNode.querySelector('.job-name').innerText;
+    const jobTitle = mainParentNode.querySelector('.job-title').innerText;
+    const salary = mainParentNode.querySelector('.salary').innerText;
+    const jobDiscription = mainParentNode.querySelector('.discription').innerText;
+    
+    let jobStatus = mainParentNode.querySelector('.status');
+    jobStatus.innerText = 'REJECTED';
+    jobStatus.className = 'w-30 text-center py-1.5 px-3 rounded-md bg-red-200 text-[14px] font-medium border-1 border-red-700 text-red-600';
+
+    const jobInfo = {
+        jobName,
+        jobTitle,
+        salary,
+        jobStatus : 'REJECTED',
+        jobDiscription
+    };
+
+    
+    const jobExist = rejectedData.find(item => item.jobName == jobInfo.jobName);
+    if (!jobExist) {
+        rejectedData.push(jobInfo);
+        renderRejected();
+    }
+    
+    interviewData = interviewData.filter(item => item.jobName !== jobInfo.jobName);
+    switchButtons();
+    
+    if (currentStatus == 'interview-btn') {
+        renderInterview();
     }
 }
-})
+});
 
 
 
@@ -94,13 +144,45 @@ function renderInterview() {
                 <h3 class="job-name text-[#002C5C] text-[20px] font-semibold">${inter.jobName}</h3>
                 <p class="job-title text-[#64748B] text-[16px]">React Native Developer</p>
                 <p class="salary text-[#64748B] text-[14px] my-[20px]">Remote • Full-time • $130,000 - $175,000</p>
-                <p class="w-30 text-center py-1.5 px-3 rounded-md bg-green-200 text-[14px] font-medium border-1 border-[#10B981] text-[#027a24]">${inter.jobStatus}</p>
+                <p class="status w-30 text-center py-1.5 px-3 rounded-md bg-green-200 text-[14px] font-medium border-1 border-[#10B981] text-[#027a24]">${inter.jobStatus}</p>
                 <p class="discription text-[#323B49] text-[14px] mt-2 mb-5">Build cross-platform mobile applications using React
                     Native. Work on products used by millions of users worldwide.</p>
                 <button
-                    class="py-1.5 px-3 border-1 rounded-md border-[#10B981] text-[#10B981] text-[14px] font-semibold cursor-pointer">INTERVIEW</button>
+                    class="interv-btn py-1.5 px-3 border-1 rounded-md border-[#10B981] text-[#10B981] text-[14px] font-semibold cursor-pointer">INTERVIEW</button>
                 <button
-                    class="py-1.5 px-3 border-1 rounded-md border-[#EF4444] text-[#EF4444] text-[14px] font-semibold cursor-pointer">REJECTED</button>
+                    class="reject-btn py-1.5 px-3 border-1 rounded-md border-[#EF4444] text-[#EF4444] text-[14px] font-semibold cursor-pointer">REJECTED</button>
+            </div>
+            <div
+                class="w-9 h-9 border-1 p-2 border-[#c7c7c7] flex items-center justify-center rounded-full text-gray-500 cursor-pointer">
+                <i class="fa-regular fa-trash-can"></i>
+            </div>
+        `
+        filterSection.appendChild(div);
+    }
+}
+
+
+
+// Render Rejected Data
+function renderRejected() {
+    filterSection.innerHTML = '';
+
+    for(let inter of rejectedData) {
+        console.log(inter);
+        let div = document.createElement('div');
+        div.className = 'bg-white p-6 rounded-lg flex justify-between';
+        div.innerHTML = `
+        <div>
+                <h3 class="job-name text-[#002C5C] text-[20px] font-semibold">${inter.jobName}</h3>
+                <p class="job-title text-[#64748B] text-[16px]">React Native Developer</p>
+                <p class="salary text-[#64748B] text-[14px] my-[20px]">Remote • Full-time • $130,000 - $175,000</p>
+                <p class="status w-30 text-center py-1.5 px-3 rounded-md text-[14px] font-medium bg-red-200 border border-red-700 text-red-600">${inter.jobStatus}</p>
+                <p class="discription text-[#323B49] text-[14px] mt-2 mb-5">Build cross-platform mobile applications using React
+                    Native. Work on products used by millions of users worldwide.</p>
+                <button
+                    class="interv-btn py-1.5 px-3 border-1 rounded-md border-[#10B981] text-[#10B981] text-[14px] font-semibold cursor-pointer">INTERVIEW</button>
+                <button
+                    class="reject-btn py-1.5 px-3 border-1 rounded-md border-[#EF4444] text-[#EF4444] text-[14px] font-semibold cursor-pointer">REJECTED</button>
             </div>
             <div
                 class="w-9 h-9 border-1 p-2 border-[#c7c7c7] flex items-center justify-center rounded-full text-gray-500 cursor-pointer">
